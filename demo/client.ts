65830d05f3cd34f129b121780de60bee553f21b4
loveencounterflow <mail@somewhere.com>
2022-03-26 18:37:17 +0100

    set font sizes

--- demo/client.ts
+++ demo/client.ts
@@ -172,6 +172,7 @@ function createTerminal(): void {
   const isWindows = ['Windows', 'Win16', 'Win32', 'WinCE'].indexOf(navigator.platform) >= 0;
   term = new Terminal({
     windowsMode: isWindows,
+    fontSize: 15,
     fontFamily: 'flow, monospace'
     // fontFamily: 'Iosevka Slab, courier-new, courier, monospace'
     // fontFamily: 'Fira Code, courier-new, courier, monospace'
